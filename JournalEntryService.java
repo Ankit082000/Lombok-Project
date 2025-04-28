14027f9cafe8f3130b452fe73e4b0d1b1bde879b
Ankit082000 <ankitrathore082000@gmail.com>
2025-04-28 22:16:13 +0530

    Add files via upload

---
+++ JournalEntryService.java
@@ -0,0 +1,59 @@
+package com.springboot.JournalApp.service;
+
+import com.springboot.JournalApp.entity.JournalEntry;
+import com.springboot.JournalApp.entity.User;
+import com.springboot.JournalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+
+@Component
+@Service
+
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    @Autowired
+    private UserService userService;
+
+
+
+    public void saveEntry(JournalEntry journalEntry, String userName){
+        User user = userService.findByUserName(userName);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry saved = journalEntryRepository.save(journalEntry);
+        user.getJournalEntries().add(saved);
+        userService.saveEntry(user);
+    }
+
+
+    public void saveEntry(JournalEntry journalEntry){
+
+        journalEntryRepository.save(journalEntry);
+    }
+
+
+    public List<JournalEntry> getAll(){
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id, String userName){
+        User user = userService.findByUserName(userName);
+        user.getJournalEntries().removeIf(journalEntry -> journalEntry.getId().equals(id));
+        userService.saveEntry(user);
+
+        journalEntryRepository.deleteById(id);
+    }
+}
